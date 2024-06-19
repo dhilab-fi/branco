a576f90945cb62e7bf225d5eaf42a5b439299160
ManueleVeggi <manuele.veggi@studio.unibo.it>
2024-06-19 16:34:02 +0200

    validate ttl and extend for cqs

--- test/triples-manager.py
+++ test/triples-manager.py
@@ -20,13 +20,6 @@ def replace_prefix_in_g(graph):
         g_with_prefixes.add((s_prefixed, p_prefixed, o_prefixed))
     return g_with_prefixes
 
-def sparql_query(graph, query):
-    results = graph.query(query)
-    for row in results:
-        #print(f"Subject: {row.subject}, Predicate: {row.predicate}, Object: {row.object}")
-        print(f"{row.subject}\n{row.predicate}\n{row.object}\n\n")
-    return results
-
 def gephi_converter(graph):
     # Create a NetworkX graph
     G = nx.DiGraph()
@@ -43,6 +36,13 @@ def gephi_converter(graph):
     print("File graph.gexf has been created.")
     return G
 
+def sparql_query(graph, cq):
+    query = "\n".join([prefix, cq])
+    for result in graph.query(query):
+        #print(type(result))
+        for el in result:
+            print(el)
+        print("\n")
 # Create a graph
 g = rdflib.Graph()
 
@@ -52,14 +52,49 @@ g.parse("de-marchi.ttl", format="ttl")
 g = replace_prefix_in_g(g)
 
 # Define the SPARQL query
-query = """
-PREFIX ex: <http://example.org/>
-SELECT ?subject ?predicate ?object
-WHERE {
-  ?subject ?predicate ?object .
-}
-LIMIT 10
+
+prefix = """
+PREFIX : <http://w3id.org/cnr-ispc/brancacci/example>
+PREFIX branco: <https://w3id.org/cnr-ispc/ontology/branco#>
+
+PREFIX crm: <http://www.cidoc-crm.org/cidoc-crm/>
+PREFIX crmsci: <http://www.ics.forth.gr/isl/CRMsci/>
+PREFIX crmdig: <http://www.ics.forth.gr/isl/CRMdig/>
+PREFIX vir: <http://w3id.org/vir#>
+
+PREFIX cito: <http://purl.org/spar/cito/>
+PREFIX fabio: <http://purl.org/spar/fabio/>
+PREFIX frbr: <http://purl.org/vocab/frbr/core#>
+PREFIX hico: <http://purl.org/emmedi/hico/>
+
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX prov: <http://www.w3.org/ns/prov#>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+PREFIX viaf: <https://viaf.org/viaf/>
 """
 
-sparql_query(g, query)
-#gephi_converter(g)
+cq1 = """
+SELECT ?obj
+WHERE {:restoration-1984 crmsci:O2_removed ?obj}
+"""
+
+cq2 = """
+SELECT ?doc 
+WHERE {?attribution crm:P177_assigned_property_of_type branco:iconography . ?attribution cito:citesAsSourceDocument ?doc. }
+"""
+
+cq3 = """
+SELECT ?icon ?absent_in_doc
+WHERE {?attribution crm:P177_assigned_property_of_type branco:iconography . ?attribution cito:extends ?absent_in_doc. ?attribution crm:P141_assigned ?icon .}
+"""
+
+cq4 = """
+SELECT ?iconology
+WHERE {?attribution crm:P177_assigned_property_of_type :iconology . ?attribution crm:P141_assigned ?iconology .}
+"""
+
+sparql_query(g, cq2)
+#sparql_query(g, "SELECT * WHERE {?s ?p ?o}")
